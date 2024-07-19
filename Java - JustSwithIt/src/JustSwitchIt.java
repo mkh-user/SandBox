c6e97f137c73772c61d984672fbfd91f6e0f31c9
Mahan Khalili <khalili1388mahan@gmail.com>
2024-07-19 15:25:16 +0330

    Create a switch for test with java

---
+++ Java - JustSwithIt/src/JustSwitchIt.java
@@ -0,0 +1,26 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class JustSwitchIt {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        out.print("Which verse? ");
+        int verse = keyboard.nextInt();
+        switch (verse) {
+            case 1:
+                out.println("That's because he has no brain.");
+                break;
+            case 2:
+                out.println("That's because he is pain.");
+                break;
+            case 3:
+                out.println("Cause this is the last refrain.");
+                break;
+            default:
+                out.println("No such verse. Please try again.");
+                break;
+        }
+        out.println("Ohhhhhh....");
+        keyboard.close();
+    }
+}
