543bb2d2d1717e85a88d3b0de88f7d62b6f52933
Mahan Khalili <khalili1388mahan@gmail.com>
2024-07-19 15:26:25 +0330

    Create simple java code for test

---
+++ Java - Millionaire/src/Millionaire.java
@@ -0,0 +1,10 @@
+public class Millionaire {
+    public static void main(String[] args) {
+        double amountInAccount;
+        amountInAccount = 50.22;
+        amountInAccount += 1000000.00;
+        System.out.print("You have $");
+        System.out.print(amountInAccount);
+        System.out.println(" in your account.");
+    }
+}
