c4997e46bd612a1212421d66c33c0994f15d977a
Mahan Khalili <khalili1388mahan@gmail.com>
2024-07-19 15:18:06 +0330

    Test character upper in java

---
+++ Java - CharUpper/src/CharDemo.java
@@ -0,0 +1,7 @@
+public class CharDemo {
+    public static void main(String[] args) {
+        char myLittleChar = 'a';
+        char myBigChar = Character.toUpperCase(myLittleChar);
+        System.out.println(myBigChar);
+    }
+}
